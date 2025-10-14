e2e8d11d5cd9da9f6c305e63ef5bc0a5c05e2ef8
harshal0004 <harshal.dhamale@elastikteams.com>
2025-10-14 09:14:47 +0530

    Solved in optimized way

--- Arrays/SellStock121.cpp
+++ Arrays/SellStock121.cpp
@@ -54,3 +54,74 @@ public:
         
     }
 };
+
+// Tried so many approaches then i came to below code to solve in O(n) but it failing for few testcases
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        if(prices.empty() || prices.size()==1)
+            return 0;
+
+        pair<int,int> minContainer;
+        int j, max=0, diff;
+        minContainer.first=0;
+        minContainer.second=prices[0];
+        int min=prices[0], index;
+
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i]<min){
+                minContainer.first = i;
+                minContainer.second = prices[i];
+                min=prices[i];
+            }
+               
+        }
+
+        j=minContainer.first+1;
+
+        while(j<prices.size()){
+            diff=prices[j]-minContainer.second;
+            if(diff>max)
+                max=diff;
+            j++;
+        }
+
+        if(max>0)
+            return max;
+        
+        return 0;
+        
+    }
+};
+
+
+// Finally after so many tries I got to the below solution by taking hints from chatgpt uff
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        if(prices.empty() || prices.size()==1)
+            return 0;
+            
+        int min_price=prices[0];
+        int max_profit=0,diff;
+
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i]<min_price){
+                min_price=prices[i];
+                continue;
+            }
+
+            diff=prices[i]-min_price;
+            
+            if(diff>max_profit)
+                max_profit=diff;
+                   
+        }
+
+        return max_profit;
+        
+    }
+};
