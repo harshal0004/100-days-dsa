e68e7a8881354dbd9aeb68d450b1bc43086cf3be
harshal0004 <harshal.dhamale@elastikteams.com>
2025-10-22 12:01:37 +0530

    Implement optimized two-pointer solution for Container With Most Water problem

--- Arrays/ContainerWithMostWaterLC11.cpp
+++ Arrays/ContainerWithMostWaterLC11.cpp
@@ -28,3 +28,31 @@ public:
         return maximumArea;
     }
 };
+
+
+// finally I solve the optimal approach for this problem by my self hurray using two pointers
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int maximumArea=0,length, breath;
+        int i=0, j=n-1;
+        while(i<j){
+            if(height[i]<height[j]){
+                length=height[i];
+                breath=j-i;
+                maximumArea=max(maximumArea, length*breath);
+                i++;
+            }else if(height[i]>=height[j]){
+                length=height[j];
+                breath=j-i;
+                maximumArea=max(maximumArea, length*breath);
+                j--;
+            }
+        }
+
+        return maximumArea;
+        
+    }
+};
